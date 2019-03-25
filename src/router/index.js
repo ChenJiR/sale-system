2bb38642920e0dbd107930b284da2a0a7924795d
chenjiarui <chenjr2@immatchu.com>
2019-03-25 15:08:12 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Frame from '@/components/Frame'
 import HelloWorld from '@/components/HelloWorld'
 import TestTable from '@/components/TestTable'
 
@@ -14,7 +15,19 @@ const routes = [
   {
     path: '/TestTable',
     component: TestTable
-  }
+  },
+  {
+    path: '/',
+    component: Frame,
+    name: '导航一',
+    iconCls: 'el-icon-message',//图标样式class
+    children: [
+      { path: '/TestTable', component: TestTable, name: 'TestTable', hidden: true },
+      // { path: '/table', component: Table, name: 'Table' },
+      // { path: '/form', component: Form, name: 'Form' },
+      // { path: '/user', component: user, name: '列表' },
+    ]
+  },
 ]
 
 export default new Router({routes})
