6865c4400ff1ec9227d3baa5c10323ee50208fa5
chenjiarui <chenjr2@immatchu.com>
2019-03-26 14:21:07 +0800

    c

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import global from '@/utils/global'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
@@ -10,10 +12,13 @@ Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 
+Vue.prototype.global = global
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
